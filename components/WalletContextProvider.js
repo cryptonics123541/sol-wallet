9f1dd3c70e530e646442e1703a8d6a8cd1c100e9
cryptonics123541 <wilshirecrypto@gmail.com>
2024-12-02 17:50:25 -0600

    1

--- components/WalletContextProvider.js
+++ components/WalletContextProvider.js
@@ -3,13 +3,13 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { Connection } from '@solana/web3.js';
 
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 export default function WalletContextProvider({ children }) {
-  const endpoint = "https://mainnet.helius-rpc.com/?api-key=4a0035ea-34b5-4afc-87a5-fbcca3474880";
-  
+  // Get the endpoint from environment variable
+  const endpoint = process.env.NEXT_PUBLIC_RPC_ENDPOINT;
+
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
