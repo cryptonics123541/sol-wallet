1cd5ac37a59b9bf95e10e7be7f1893655fd7d302
cryptonics123541 <wilshirecrypto@gmail.com>
2024-12-02 18:10:05 -0600

    1

--- utils/db.js
+++ utils/db.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
   if (mongoose.connections[0].readyState) return;
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     if (process.env.NODE_ENV !== 'production') {
       console.log('MongoDB connected');
