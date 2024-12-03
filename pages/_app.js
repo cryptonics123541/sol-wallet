53a1f61d42246cc741c84c1e8d632e8c8d7d09da
cryptonics123541 <wilshirecrypto@gmail.com>
2024-12-03 10:11:46 -0600

    123

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,26 @@
 import '../styles/globals.css';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import ClientWalletProvider from '../components/WalletProvider';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <ClientWalletProvider>
-            <Component {...pageProps} />
-        </ClientWalletProvider>
+        <>
+            <Head>
+                <title>Sol Token Burner</title>
+                <meta name="description" content="A secure tool for burning Solana tokens" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                
+                {/* Phantom dApp Metadata */}
+                <meta name="title" content="Sol Token Burner" />
+                <meta name="og:title" content="Sol Token Burner" />
+                <meta name="og:description" content="A secure tool for burning Solana tokens" />
+                <meta name="og:image" content="/icon.png" />
+            </Head>
+            <ClientWalletProvider>
+                <Component {...pageProps} />
+            </ClientWalletProvider>
+        </>
     );
 }
 
