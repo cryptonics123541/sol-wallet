c9f10abe973043825743e694a8519e041797443b
cryptonics123541 <wilshirecrypto@gmail.com>
2024-12-03 23:52:08 -0600

    123

--- script.js
+++ script.js
@@ -57,5 +57,4 @@ animate();
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
+   
